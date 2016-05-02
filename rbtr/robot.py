5c0fda1828bb4221291dcfaa33ad4f57479ac7bc
Julian Kuhlmann <jlnkuhlmann@gmail.com>
2016-05-02 21:56:16 +0200

    Wrote docstrings for public methods.

--- rbtr/robot.py
+++ rbtr/robot.py
@@ -11,6 +11,12 @@ _ROTATION_MATRIX_90DEG_RIGHT = np.array([[0, 1], [-1, 0]])
 
 
 class Robot():
+    """
+    A simulated robot that moves on a 2D tabletop.
+
+    Parameters:
+    * table: an instance of the class rbtr.table.Table
+    """
     def __init__(self, table):
         self._table = table
         self._current_position = np.array([None, None])
@@ -19,23 +25,38 @@ class Robot():
 
     @property
     def current_x(self):
+        """The current x coordinate."""
         return self._current_position[0]
 
     @property
     def current_y(self):
+        """The current y coordinate."""
         return self._current_position[1]
 
     @property
     def current_facing(self):
+        """The direction in which the robot is currently facing."""
         return _direction_string(self._current_facing)
 
     def place(self, x_coordinate, y_coordinate, facing):
+        """
+        Place the robot on the tabletop.
+
+        Parameters:
+        * x_coordinate: the x coordinate at which the robot should be placed.
+        * y_coordinate: the y coordinate at which the robot should be placed.
+        * facing: the direction in which the robot should be facing;
+            one of 'NORTH', 'EAST', 'SOUTH', or 'WEST'
+        """
         if self._table.is_safe(x_coordinate=x_coordinate, y_coordinate=y_coordinate):
             self._current_position = np.array([x_coordinate, y_coordinate])
             self._current_facing = _DIRECTION_VECTORS[facing]
             self._placed = True
 
     def report(self):
+        """
+        Return a report containing the position of the robot and the direction in which it is facing.
+        """
         return "X: {}, Y: {}, F: {}".format(
             self._current_position[0],
             self._current_position[1],
@@ -43,6 +64,9 @@ class Robot():
         )
 
     def move(self):
+        """
+        Move the robot one square in the direction in which it is currently facing.
+        """
         if not self._placed:
             return
         prospective_position = self._current_position + self._current_facing
@@ -54,11 +78,13 @@ class Robot():
             print(msg.format(prospective_position[0], prospective_position[1]))
 
     def left(self):
+        """Turn the robot towards its left by 90 degrees."""
         if not self._placed:
             return
         self._current_facing = np.dot(_ROTATION_MATRIX_90DEG_LEFT, self._current_facing)
 
     def right(self):
+        """Turn the robot towards its right by 90 degrees."""
         if not self._placed:
             return
         self._current_facing = np.dot(_ROTATION_MATRIX_90DEG_RIGHT, self._current_facing)
--- rbtr/table.py
+++ rbtr/table.py
@@ -1,9 +1,17 @@
 class Table():
+    """
+    A model of a 2D tabletop.
+
+    Parameters:
+    * x_dimension: The number of squares in east-west direction.
+    * y_dimension: The number of squares in north-south direction.
+    """
     def __init__(self, x_dimension, y_dimension):
         self._x_dimension = x_dimension
         self._y_dimension = y_dimension
 
     def is_safe(self, x_coordinate, y_coordinate):
+        """Given x and y coordinates, return whether the position is safe, i.e. on the table."""
         if 0 <= x_coordinate < self._x_dimension and 0 <= y_coordinate < self._y_dimension:
             return True
         return False
